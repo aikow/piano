95b414680ba82643db80cb7575480106a9d109d7
Aiko Wessels <aiko.wessels@gmail.com>
2021-12-24 10:19:40 +0100

    Implement rough outline

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use std::error::Error;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::BufReader;
 use std::path::Path;
+use std::str::FromStr;
 
+use rand::distributions::Standard;
+use rand::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -10,6 +15,37 @@ pub enum ScaleMode {
     Major,
     Minor,
 }
+
+impl FromStr for ScaleMode {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "major" => Ok(ScaleMode::Major),
+            "minor" => Ok(ScaleMode::Minor),
+            _ => Err(())
+        }
+    }
+}
+
+impl Display for ScaleMode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            ScaleMode::Major => write!(f, "Major"),
+            ScaleMode::Minor => write!(f, "Minor"),
+        }
+    }
+}
+
+impl Distribution<ScaleMode> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ScaleMode {
+        match rng.gen_range(0..2) {
+            0 => ScaleMode::Major,
+            _ => ScaleMode::Minor,
+        }
+    }
+}
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Scale {
@@ -21,26 +57,56 @@ impl Scale {
     pub fn new(key: String, mode: ScaleMode) -> Self {
         Self { key, mode }
     }
+
+    const KEYS: [&'static str; 7] = ["A", "B", "C", "D", "E", "F", "G"];
 }
+
+impl Display for Scale {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.key, self.mode)
+    }
+}
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Song {
     name: String,
     composer: Vec<String>,
-    arranger: Option<Vec<String>>,
+    arranger: Vec<String>,
+    genre: Vec<String>,
     scale: Option<Scale>,
 }
 
 impl Song {
-    pub fn new(name: String, composer: Vec<String>, arranger: Option<Vec<String>>, scale: Option<Scale>) -> Self {
-        Self { name, composer, arranger, scale }
+    pub fn new(name: String, composer: Vec<String>, arranger: Vec<String>, genre: Vec<String>, scale: Option<Scale>) -> Self {
+        Self { name, composer, arranger, genre, scale }
     }
 }
 
-pub fn parse_config<P: AsRef<Path>>(path: P) -> Result<Vec<Song>, Box<dyn Error>>{
-    let mut file = File::open(path)?;
+
+pub fn parse_config<P: AsRef<Path>>(path: P) -> Result<Vec<Song>, Box<dyn Error>> {
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
     let deserialized_songs: Vec<Song> = serde_yaml::from_reader(reader)?;
 
     Ok(deserialized_songs)
+}
+
+pub fn pick_random_key() -> &'static str {
+    Scale::KEYS.choose(&mut rand::thread_rng()).unwrap()
+}
+
+pub fn pick_random_mode() -> ScaleMode {
+    rand::random()
+}
+
+pub fn pick_random_scale(mode: &Option<String>) -> Result<Scale, String> {
+    return if let Some(mode) = mode {
+        match ScaleMode::from_str(mode) {
+            Ok(mode) => { Ok(Scale::new(String::from(pick_random_key()), mode)) }
+            Err(_err) => { Err(String::from("Failed to parse mode from input string.")) }
+        }
+    } else {
+        Ok(Scale::new(String::from(pick_random_key()), pick_random_mode()))
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,70 @@
+use clap::{AppSettings, Parser, Subcommand};
+use piano::{pick_random_scale};
+
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+#[clap(setting(AppSettings::SubcommandRequiredElseHelp))]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Picks a random scale.
+    Scale {
+        /// Pick a scale from only the following mode.
+        #[clap(short = 'm', long = "mode")]
+        mode: Option<String>,
+    },
+
+    /// Pick a song.
+    Song {
+        /// Only pick a song from this composer.
+        #[clap(short, long)]
+        composer: Option<String>,
+
+        /// Only pick a song from this arranger.
+        #[clap(short, long)]
+        arranger: Option<String>,
+
+        /// Only pick a song from this genre.
+        #[clap(short, long)]
+        genre: Option<String>,
+
+        /// Only pick a song in this scale.
+        #[clap(short, long)]
+        scale: Option<String>,
+
+        /// If set, list all the found songs instead of picking a random one.
+        #[clap(short, long)]
+        list: bool,
+    },
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 
-    if let Ok(songs) = piano::parse_config("example.yaml") {
-        for song in &songs {
-            println!("{:?}", song);
+    match &args.command {
+        Commands::Scale { mode } => {
+            match pick_random_scale(mode) {
+                Ok(scale) => println!("Scale: {}", scale),
+                Err(err) => {
+                    println!("Failed to pick a scale: {}", err)
+                }
+            }
         }
-    } else {
-        println!("Failed to read from file");
-    }
+        Commands::Song { composer, arranger, genre, scale, list } => {
+            if let Ok(songs) = piano::parse_config("example.yaml") {
+                for song in &songs {
+                    println!("{:?}", song);
+                }
+            } else {
+                println!("Failed to read from file");
+            }
+        }
+    };
 }
